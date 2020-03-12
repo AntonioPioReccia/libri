225aa451eea01f81e8128601c141b5ef831c9ae2
antoniopioreccia <antonioreccia52011@gmail.com>
2020-03-12 20:49:55 +0100

    modifica esercizio

--- index.js
+++ index.js
@@ -6,25 +6,42 @@ app.set ("views",path.join(__dirname,"views"))
 app.set ("view engine", "ejs");
 const db=new sqlite3.Database("libri.db" , function(){
     app.listen(3000);  
-}) ;
-app.get("/", function(req, res) 
-{
+});
+app.get("/", function(req, res){
     res.sendFile(path.join(__dirname,"public", "index.html"));
-})
-app.get ("/autori", function(req,res)
-
-{
+});
+app.get("/admin", function(req, res){
+    res.sendFile(path.join(__dirname,"public", "admin.html"));
+});
+app.get ("/autori", function(req,res){
     const sql="select * from autori;"
     db.all(sql,function(err,rows) {
         console.log( rows)
         res.render("autori", {autori:rows});
-    }) 
-   
-}
-)
-app.use( function(req, res) 
-{
+    }); 
+});
+app.get ("/libri", function(req,res){
+    const sql="select * from libri;"
+    db.all(sql,function(err,rows) {
+        console.log( rows)
+        res.render("libri", {libri:rows});
+    }); 
+});
+app.get ("/admin/autori", function(req,res){
+    const sql="select * from autori;"
+    db.all(sql,function(err,rows) {
+        console.log( rows)
+        res.render("admin_autori", {autori:rows});
+    }); 
+});
+app.get ("/admin/libri", function(req,res){
+    const sql="select * from libri;"
+    db.all(sql,function(err,rows) {
+        console.log( rows)
+        res.render("admin_libri", {libri:rows});
+    }); 
+});
+app.use( function(req, res){
     res.status(404);
     res.sendFile(path.join (__dirname,"public","404.html"));
-})
-                              
+});                              
